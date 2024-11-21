881d5a91681023d1be31b2884c4880950c2a24bb
hoaivm <hoaivm@ghn.vn>
2024-11-21 14:29:58 +0700

    unit test

--- main.go
+++ main.go
@@ -6,6 +6,11 @@ import (
 	"net/http"
 )
 
+// Hàm đơn giản cần kiểm tra
+func Add(a, b int) int {
+	return a + b
+}
+
 func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello, World!")
 }
